acd19fdff3d44eba290bbc68c07bd7fa7978fc2b
christopherrons <christopherheron09@gmail.com>
2024-07-25 15:27:38 +0200

    Update to java 21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,8 +4,8 @@ include("integrations")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            library("spring.boot.starter.web", "org.springframework.boot:spring-boot-starter-web:3.0.1")
-            library("spring.boot.starter.parent", "org.springframework.boot:spring-boot-starter-parent:3.0.1")
+            library("spring.boot.starter.web", "org.springframework.boot:spring-boot-starter-web:3.2.0")
+            library("spring.boot.starter.parent", "org.springframework.boot:spring-boot-starter-parent:3.2.0")
             library("tyrus.standalone.client", "org.glassfish.tyrus.bundles:tyrus-standalone-client:1.18")
             library("javax.json.api", "javax.json:javax.json-api:1.1.4")
             library("javax.json", "org.glassfish:javax.json:1.1.4")
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
         }
 
         create("testlibs") {
-            library("spring.boot.starter.test", "org.springframework.boot:spring-boot-starter-test:3.0.1")
+            library("spring.boot.starter.test", "org.springframework.boot:spring-boot-starter-test:3.2.0")
             library("junit.jupiter.api", "org.junit.jupiter:junit-jupiter-api:5.8.1")
             library("junit.jupiter.engine", "org.junit.jupiter:junit-jupiter-engine:5.8.1")
         }
